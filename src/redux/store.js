3ac6dd680b6d6a4befe50c829ebb8e9a61141c23
Serhii-Buha <serhii.buga@gmail.com>
2023-09-20 16:57:56 +0300

    added persistSliceReducer,  persistStore, redux-persist, getDefaultMiddleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistSliceReducer } from './slice';
 import { carsApi } from './carsApi';
+
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
 
 export const store = configureStore({
   reducer: {
+    favorite: persistSliceReducer,
     [carsApi.reducerPath]: carsApi.reducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(carsApi.middleware),
+
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    carsApi.middleware,
+  ],
 });
+
+export const persistor = persistStore(store);
