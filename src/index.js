f0e0288c2e24af99441b9cf5bc41318f8d784cb5
Serhii-Buha <serhii.buga@gmail.com>
2023-09-20 11:54:16 +0300

    adding BrowserRouter, react-redux-Provider, redux/store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter basename="/project-cars">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
