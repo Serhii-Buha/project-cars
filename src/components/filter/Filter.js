2e5304bd7715a7232d79e755f248ec445c49e2e3
Serhii-Buha <serhii.buga@gmail.com>
2023-09-20 17:03:33 +0300

    refactoring of the Filter component

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,8 +1,37 @@
 export const favoriteCars = (data, favorite) => {
-  return favorite.length === 0
+  return favorite?.length === 0
     ? []
     : data?.filter(item => favorite.includes(item.id));
 };
+
+export const priceRangeCurrent = data => [
+  ...new Set(
+    data?.map(item => +item.rentalPrice.slice(1)).sort((a, b) => a - b)
+  ),
+];
+
+export const priceRange10 = data =>
+  data.length === 0
+    ? []
+    : [
+        ...Array(
+          Math.ceil(
+            (Math.max(...data?.map(item => +item.rentalPrice.slice(1))) -
+              Math.min(...data?.map(item => +item.rentalPrice.slice(1)))) /
+              10
+          ) + 1
+        ),
+      ].map(
+        (e, i) =>
+          Math.ceil(
+            Math.min(...data?.map(item => +item.rentalPrice.slice(1))) / 10
+          ) *
+            10 +
+          i * 10
+      );
+
+export const makesList = data =>
+  data?.length === 0 ? [] : [...new Set(data?.map(item => item.make))].sort();
 
 export const filterCars = (data, filter) => {
   const { brand = '', price = '', from = '', to = '' } = filter;
